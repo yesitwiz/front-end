68c580fb8c292422b5fda183a2e8f5de839a0d3d
hongb918 <hong.b918@gmail.com>
2022-05-21 10:42:10 -0500

    file structuring

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+//Header for Home Page?
+
+import React from 'react'
+
+function Header() {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
---
+++ src/components/user/User.js
@@ -0,0 +1,1 @@
+//React component User to be exported to Charger
